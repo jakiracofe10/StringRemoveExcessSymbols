a9649474b14c0b775824c9aace69778b2fc5bdec
Jason <jakiracofe10@gmail.com>
2016-12-01 11:41:12 -0600

    comments

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ using namespace std;
 //Space: O(1)
 
 string removeExcess(string s) {
+    //start at -1 to clarify erase call
     int spaceCount = -1;
     for (int i=0; i<s.length(); i++) {
         if (s[i] == ' ') {
             spaceCount++;
         } else {
             if (spaceCount > 0) {
+                /*remove all indexes (spaces) after first space length equal to number of spaces
+                before next non space index*/
                 s.erase(i-spaceCount, spaceCount);
             }
             spaceCount = -1;
